c857e068eb4ed329ed43b20904ee2b8d35e444b9
ankitkkataria <ankitkkataria@gmail.com>
2025-05-27 14:03:32 +0530

    really happy with this

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -139,6 +139,7 @@ return {
       require("configs.cmp").setup()
     end,
   },
+
   {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
