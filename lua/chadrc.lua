5208a7b97c4cf31928d1d5dedd369f3028d7caf7
ankitkkataria <ankitkkataria@gmail.com>
2025-05-29 03:57:14 +0530

    Committing the final landing page

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -51,34 +51,33 @@ M.nvdash = {
   },
 
   buttons = {
-    { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
-    { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
-    { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" },
+    { txt = "  Find Files          ", keys = "ff", cmd = "Telescope find_files" },
+    { txt = "  Recent Files        ", keys = "fo", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word           ", keys = "fw", cmd = "Telescope live_grep" },
     {
-      txt = "  File Explorer",
+      txt = "  File Explorer       ",
       keys = "m",
       cmd = ":lua require('mini.files').open(vim.loop.cwd(), false)<CR>",
     },
     {
-      txt = "  Sessions",
+      txt = "  Sessions            ",
       keys = "sa",
       cmd = ":lua require('persistence').select()",
     },
     -- { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" },
     -- { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" },
-    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
-
-    {
-      txt = function()
-        local stats = require("lazy").stats()
-        local ms = math.floor(stats.startuptime) .. " ms"
-        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-      end,
-      hl = "NvDashFooter",
-      no_gap = true,
-    },
-
-    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    --
+    -- {
+    --   txt = function()
+    --     local stats = require("lazy").stats()
+    --     local ms = math.floor(stats.startuptime) .. " ms"
+    --     return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+    --   end,
+    --   hl = "NvDashFooter",
+    --   no_gap = true,
+    -- },
+    --
+    -- { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
   },
 }
 
