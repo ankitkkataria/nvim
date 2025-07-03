07724d7715711111fab17cd66f11c28f7f514736
ankitkkataria <ankitkkataria@gmail.com>
2025-07-04 00:01:41 +0530

    just disabled neoclip it was causing long bootup times due to sql-lite storage

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -1,104 +1,105 @@
 return {
-  "AckslD/nvim-neoclip.lua",
-  dependencies = {
-    { "nvim-telescope/telescope.nvim" },
-    { "kkharji/sqlite.lua" },
-  },
-  config = function()
-    require("neoclip").setup {
-      history = 1000,
-      enable_persistent_history = true,
-      length_limit = 1048576,
-      continuous_sync = false,
-      db_path = vim.fn.stdpath "data" .. "/databases/neoclip.sqlite3",
-      filter = nil,
-      preview = true,
-      prompt = nil,
-      -- default_register = '"', -- Changed to system clipboard register
-      default_register_macros = "q",
-      enable_macro_history = true,
-      content_spec_column = false,
-      disable_keycodes_parsing = false,
-      on_select = {
-        move_to_front = false,
-        close_telescope = true,
-      },
-      on_paste = {
-        set_reg = true,
-        move_to_front = false,
-        close_telescope = true,
-      },
-      on_replay = {
-        set_reg = false,
-        move_to_front = false,
-        close_telescope = true,
-      },
-      on_custom_action = {
-        close_telescope = true,
-      },
-      keys = {
-        telescope = {
-          i = {
-            select = "<cr>",
-            paste = "<c-j>",
-            paste_behind = "<c-k>",
-            replay = "<c-q>",
-            delete = "<c-d>",
-            edit = "<c-e>",
-            custom = {},
-          },
-          n = {
-            select = "<cr>",
-            paste = "p",
-            paste_behind = "P",
-            replay = "q",
-            delete = "d",
-            edit = "e",
-            custom = {},
-          },
-        },
-      },
-    }
-
-    -- Configure telescope extension to copy to system clipboard on Enter
-    require("telescope").setup {
-      extensions = {
-        neoclip = {
-          mappings = {
-            i = {
-              ["<CR>"] = function(prompt_bufnr)
-                local selection = require("telescope.actions.state").get_selected_entry()
-                if selection then
-                  -- Copy to system clipboard (both + and * registers)
-                  vim.fn.setreg("+", selection.value)
-                  vim.fn.setreg("*", selection.value)
-                  -- Also set the default register
-                  vim.fn.setreg('"', selection.value)
-                  print("Copied to system clipboard: " .. string.sub(selection.value, 1, 50) .. "...")
-                end
-                require("telescope.actions").close(prompt_bufnr)
-              end,
-            },
-            n = {
-              ["<CR>"] = function(prompt_bufnr)
-                local selection = require("telescope.actions.state").get_selected_entry()
-                if selection then
-                  -- Copy to system clipboard (both + and * registers)
-                  vim.fn.setreg("+", selection.value)
-                  vim.fn.setreg("*", selection.value)
-                  -- Also set the default register
-                  vim.fn.setreg('"', selection.value)
-                  print("Copied to system clipboard: " .. string.sub(selection.value, 1, 50) .. "...")
-                end
-                require("telescope.actions").close(prompt_bufnr)
-              end,
-            },
-          },
-        },
-      },
-    }
-    -- Load the extension after setup
-    require("telescope").load_extension "neoclip"
-    vim.keymap.set("n", "<leader>cb", "<cmd>Telescope neoclip<CR>", { desc = "Telescope Neoclip" })
-  end,
+  -- "AckslD/nvim-neoclip.lua",
+  -- dependencies = {
+  --   { "nvim-telescope/telescope.nvim" },
+  --   { "kkharji/sqlite.lua" },
+  -- },
+  -- event = "VeryLazy",
+  -- config = function()
+  --   require("neoclip").setup {
+  --     history = 50,
+  --     enable_persistent_history = true,
+  --     length_limit = 1048576,
+  --     continuous_sync = false,
+  --     db_path = vim.fn.stdpath "data" .. "/databases/neoclip.sqlite3",
+  --     filter = nil,
+  --     preview = true,
+  --     prompt = nil,
+  --     -- default_register = '"', -- Changed to system clipboard register
+  --     default_register_macros = "q",
+  --     enable_macro_history = true,
+  --     content_spec_column = false,
+  --     disable_keycodes_parsing = false,
+  --     on_select = {
+  --       move_to_front = false,
+  --       close_telescope = true,
+  --     },
+  --     on_paste = {
+  --       set_reg = true,
+  --       move_to_front = false,
+  --       close_telescope = true,
+  --     },
+  --     on_replay = {
+  --       set_reg = false,
+  --       move_to_front = false,
+  --       close_telescope = true,
+  --     },
+  --     on_custom_action = {
+  --       close_telescope = true,
+  --     },
+  --     keys = {
+  --       telescope = {
+  --         i = {
+  --           select = "<cr>",
+  --           paste = "<c-j>",
+  --           paste_behind = "<c-k>",
+  --           replay = "<c-q>",
+  --           delete = "<c-d>",
+  --           edit = "<c-e>",
+  --           custom = {},
+  --         },
+  --         n = {
+  --           select = "<cr>",
+  --           paste = "p",
+  --           paste_behind = "P",
+  --           replay = "q",
+  --           delete = "d",
+  --           edit = "e",
+  --           custom = {},
+  --         },
+  --       },
+  --     },
+  --   }
+  --
+  --   -- Configure telescope extension to copy to system clipboard on Enter
+  --   require("telescope").setup {
+  --     extensions = {
+  --       neoclip = {
+  --         mappings = {
+  --           i = {
+  --             ["<CR>"] = function(prompt_bufnr)
+  --               local selection = require("telescope.actions.state").get_selected_entry()
+  --               if selection then
+  --                 -- Copy to system clipboard (both + and * registers)
+  --                 vim.fn.setreg("+", selection.value)
+  --                 vim.fn.setreg("*", selection.value)
+  --                 -- Also set the default register
+  --                 vim.fn.setreg('"', selection.value)
+  --                 print("Copied to system clipboard: " .. string.sub(selection.value, 1, 50) .. "...")
+  --               end
+  --               require("telescope.actions").close(prompt_bufnr)
+  --             end,
+  --           },
+  --           n = {
+  --             ["<CR>"] = function(prompt_bufnr)
+  --               local selection = require("telescope.actions.state").get_selected_entry()
+  --               if selection then
+  --                 -- Copy to system clipboard (both + and * registers)
+  --                 vim.fn.setreg("+", selection.value)
+  --                 vim.fn.setreg("*", selection.value)
+  --                 -- Also set the default register
+  --                 vim.fn.setreg('"', selection.value)
+  --                 print("Copied to system clipboard: " .. string.sub(selection.value, 1, 50) .. "...")
+  --               end
+  --               require("telescope.actions").close(prompt_bufnr)
+  --             end,
+  --           },
+  --         },
+  --       },
+  --     },
+  --   }
+  --   -- Load the extension after setup
+  --   require("telescope").load_extension "neoclip"
+  --   vim.keymap.set("n", "<leader>cb", "<cmd>Telescope neoclip<CR>", { desc = "Telescope Neoclip" })
+  -- end,
 }
