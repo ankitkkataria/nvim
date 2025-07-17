ea415323dc36f3a1304d6c42b6c542dea2f235a6
ankitkkataria <ankitkkataria@gmail.com>
2025-07-17 21:43:52 +0530

    changed floating terminal's size

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -93,10 +93,10 @@ M.term = {
   sizes = { sp = 0.3, vsp = 0.35, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
   float = {
     relative = "editor",
-    row = 0.3,
-    col = 0.25,
-    width = 0.5,
-    height = 0.4,
+    row = 0.05,
+    col = 0.1,
+    width = 0.8,
+    height = 0.8,
     border = "single",
   },
 }
